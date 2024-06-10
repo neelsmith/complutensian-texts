7cdbe8493a5a82327d257fc3acf36c29e6e16de4
Neel Smith <dnsmith.neel@gmail.com>
2024-06-10 13:52:30 -0400

    Add placeholder for new notebook

---
+++ pluto/latinmorph.jl
@@ -0,0 +1,16 @@
+### A Pluto.jl notebook ###
+# v0.19.41
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0abfe5ae-2752-11ef-38cf-f984b91b0112
+begin
+end
+
+# ╔═╡ 2d774dd8-0513-4957-9b6a-5a33fd300199
+md"""# Morphology of Latin verbs in Complutensian Bible"""
+
+# ╔═╡ Cell order:
+# ╠═0abfe5ae-2752-11ef-38cf-f984b91b0112
+# ╠═2d774dd8-0513-4957-9b6a-5a33fd300199
