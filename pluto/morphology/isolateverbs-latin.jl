ac8d7a03548322b3f6c45fbf13f38340e9dd767f
Neel Smith <dnsmith.neel@gmail.com>
2024-12-08 14:47:27 -0500

    NBs

--- pluto/morphology/isolateverbs-latin.jl
+++ pluto/morphology/isolateverbs-latin.jl
@@ -1,17 +1,19 @@
 ### A Pluto.jl notebook ###
-# v0.19.47
+# v0.20.3
 
 using Markdown
 using InteractiveUtils
 
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
+    #! format: off
     quote
         local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
         local el = $(esc(element))
         global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
         el
     end
+    #! format: on
 end
 
 # ╔═╡ f82ebfde-453c-4e47-bfeb-df73c7633059
@@ -306,7 +308,7 @@ Tabulae = "~0.15.0"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.10.6"
+julia_version = "1.10.7"
 manifest_format = "2.0"
 project_hash = "015723d8dbf1ef792475d0148595d88f35315559"
 
