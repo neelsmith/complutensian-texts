ecfdaab6c0f6f7d589c356b66b485f86b41f1fc4
Neel Smith <dnsmith.neel@gmail.com>
2026-02-24 14:32:34 -0500

    Progress on purepython nb

--- nbs-purepython/readglosses.py
+++ nbs-purepython/readglosses.py
@@ -36,6 +36,30 @@ def _(bookchoice, chapterchoice, mo, passagechoice):
 
 
 @app.cell(hide_code=True)
+def _(bookchoice, lxxdisplaychoices, mo, passagechoice):
+    lxxdisplay = None
+    if passagechoice.value:
+        lxxdisplay = mo.md(f"""## Latin glosses to Septuagint: : *{bookchoice.value.title()}*, {passagechoice.value}
+
+    *Display* {lxxdisplaychoices}
+
+
+    """)
+
+    lxxdisplay    
+    return (lxxdisplay,)
+
+
+@app.cell(hide_code=True)
+def _(lxxblocks, lxxdisplay, mo):
+    showlxx = None
+    if lxxdisplay:
+        showlxx = mo.hstack(lxxblocks)
+    showlxx    
+    return
+
+
+@app.cell(hide_code=True)
 def _(mo):
     mo.Html("<hr/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>")
     return
@@ -47,6 +71,125 @@ def _(mo):
     ## Computation
     """)
     return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    ### Display blocks
+    """)
+    return
+
+
+@app.cell
+def _(lxxdisplaychoices):
+    lxxdisplaychoices.value
+    return
+
+
+@app.cell
+def _(lxxdipltext, lxxdisplaychoices, lxxnormtext, mo):
+    lxxblocks = []
+    lxxcandidates = [
+      "lxxdiplresult",
+      "lxxnormresult",
+      "lxxdiffs"
+    ]
+    if lxxnormtext:
+        if "lxxdiplresult" in lxxdisplaychoices.value:
+            lxxblocks.append(mo.vstack([mo.md("**Diplomatic text**"), mo.md(lxxdipltext)]))
+        if "lxxnormresult" in lxxdisplaychoices.value:
+            lxxblocks.append(mo.vstack([mo.md("**Normalized text**"), mo.md(lxxnormtext)]))   
+        #if "lxxdiffs" in lxxdisplaychoices.value:
+        #    lxxblocks.append(mo.vstack([mo.md("**Difference**"), mo.md(lxxdiffs)]))     
+    return (lxxblocks,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    ### Display choices
+    """)
+    return
+
+
+@app.cell
+def _(lxxdisplayblocks, mo):
+    lxxdisplaychoices = mo.ui.multiselect(options = lxxdisplayblocks, value = list(lxxdisplayblocks.keys()))
+    return (lxxdisplaychoices,)
+
+
+@app.cell
+def _(mo, targumdisplayblocks):
+    targumdisplaychoices = mo.ui.multiselect(options = targumdisplayblocks, value = list(targumdisplayblocks.keys()))
+    return
+
+
+@app.cell
+def _():
+    lxxdisplayblocks = {
+        "Septuagint glosses: diplomatic text": "lxxdiplresult",
+        "Septuagint glosses: normalized": "lxxnormresult",
+        "Septuagint glosses: visual diff": "lxxdiffs"
+    }
+    return (lxxdisplayblocks,)
+
+
+@app.cell
+def _():
+    targumdisplayblocks = {
+        "Targum glosses: diplomatic text": "targumdiplresult",
+        "Targum glosses: normalized": "targumnormresult",
+        "Targum glosses: visual diff": "targumdiffs"
+    }
+    return (targumdisplayblocks,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    ### Text selection
+    """)
+    return
+
+
+@app.cell
+def _(lxxdipl, lxxnorm, passagechoice, pl, targumdipl, targumnorm):
+    def lookup_passages():
+        """Get text contents of passages matching user's current selection."""
+        lxxglossurnbase = "urn:cts:compnov:bible.genesis.sept_latin"
+        targumglossurnbase = "urn:cts:compnov:bible.genesis.targum_latin"
+    
+        if passagechoice.value:
+            lxxdipl_urn = lxxglossurnbase + ".diplomatic:" + passagechoice.value
+            lxxdiplrows = lxxdipl.filter(pl.col("urn") == lxxdipl_urn).get_column("text")
+            lxxdipltext = "\n".join(lxxdiplrows)
+
+            lxxnorm_urn = lxxglossurnbase + ".normalized:" + passagechoice.value
+            lxxnormrows = lxxnorm.filter(pl.col("urn") == lxxnorm_urn).get_column("text")
+            lxxnormtext = "\n".join(lxxnormrows)
+
+            targumdipl_urn = targumglossurnbase + ".diplomatic:" + passagechoice.value
+            targumdiplrows = targumdipl.filter(pl.col("urn") == targumdipl_urn).get_column("text")
+            targumdipltext = "\n".join(targumdiplrows)
+
+            targumnorm_urn = targumglossurnbase + ".normalized:" + passagechoice.value
+            targumnormrows = targumnorm.filter(pl.col("urn") == targumnorm_urn).get_column("text")
+            targumnormtext = "\n".join(targumnormrows)
+        
+        
+            return [lxxdipltext, lxxnormtext, targumdipltext, targumnormtext]
+        
+        else:
+            return [None,None, None, None]
+
+    return (lookup_passages,)
+
+
+@app.cell
+def _(lookup_passages):
+    lxxdipltext, lxxnormtext, targumdipltext, targumnormtext = lookup_passages()
+    return lxxdipltext, lxxnormtext
 
 
 @app.cell(hide_code=True)
@@ -132,7 +275,7 @@ def _(mo):
 @app.cell
 def _(readeditions):
     lxxdipl, lxxnorm, targumdipl, targumnorm = readeditions()
-    return (lxxdipl,)
+    return lxxdipl, lxxnorm, targumdipl, targumnorm
 
 
 @app.cell
