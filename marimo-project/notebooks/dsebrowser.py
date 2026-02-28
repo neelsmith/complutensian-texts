6f511e23b5ef8f84cc9a880367f3e84390c6e60c
Neel Smith <dnsmith.neel@gmail.com>
2026-02-28 08:38:31 -0500

    WOrk on dsebrowserr

--- marimo-project/notebooks/dsebrowser.py
+++ marimo-project/notebooks/dsebrowser.py
@@ -1,12 +1,3 @@
-# /// script
-# dependencies = [
-#     "dse-polars==0.1.1",
-#     "iiif-anywidget==0.3.0",
-#     "marimo",
-# ]
-# requires-python = ">=3.14"
-# ///
-
 import marimo
 
 __generated_with = "0.20.2"
@@ -21,51 +12,25 @@ def _():
 
 
 @app.cell
-def _(info_url2urn, service, ustate):
-    currentimage = info_url2urn(ustate["url"],service)
-    return (currentimage,)
-
-
-@app.cell
-def _(currentimage, lxx, pl):
-    imagematches = (
-        lxx.df
-        .filter(pl.col("image").cast(pl.Utf8).str.starts_with(currentimage))
-        .with_columns(
-            pl.col("image").cast(pl.Utf8).str.replace(r"^.*@", "").alias("roi")
-        )
-    )
-    return (imagematches,)
-
-
-@app.cell
-def _(getroi, imagematches):
-    currentrois = imagematches.with_columns(getroi())["roi"].to_list()
-    return (currentrois,)
-
-
-@app.cell
-def _(currentimage, lxx):
-    currentpassages = lxx.passagesforimage(currentimage)
+def _():
+    #dse.df
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md("""
-    # Browse DSE of the Complutensian Polyglot Bible
-    """)
-    return
+    debug = mo.ui.checkbox()
+    mo.md(f"*Show debugging info* {debug}")
+    return (debug,)
 
 
 @app.cell(hide_code=True)
-def _(info_url2urn, mo, service, ustate):
-    mo.vstack([
-        mo.md("*Currently displayed image*:"),
-        mo.md(f"""{ustate["url"]}"""),
-        mo.md(f"*Equivalent URN*:"),
-        mo.md(f"`{info_url2urn(ustate["url"],service)}`")
-    ])
+def _(mo):
+    mo.md("""
+    # Browse the Complutensian polyglot Bible
+
+    Latin glosses to the Septuagint and Targum Onkelos have been manually edited for *Genesis*. Browse in volume 1 to read these transcriptions alongside the source image.
+    """)
     return
 
 
@@ -76,37 +41,57 @@ def _(manifest):
 
 
 @app.cell(hide_code=True)
-def _(imageheight):
-    imageheight
+def _(
+    clickedon,
+    currentpsgreff,
+    debug,
+    imgstate,
+    manifest,
+    mo,
+    x_state,
+    y_state,
+):
+    showinfo = None
+    if debug.value:
+        showinfo = mo.vstack([
+            mo.vstack([
+                mo.md("*Manifest*:"),
+                mo.md(f"{manifest.value}")]),
+            mo.vstack([mo.md("*Current image*:"), mo.md(f"{imgstate}")]),
+            mo.md(f"x,y =  `{x_state}, {y_state}`"),
+
+            mo.vstack([mo.md("*Clicked on*"), clickedon]),
+            mo.vstack([mo.md("*Text reff"), currentpsgreff])
+        ])
+    showinfo    
     return
 
 
-@app.cell(hide_code=True)
-def _(IIIFImageOverlayViewer, currentrois, ustate):
-    overlayviewer = IIIFImageOverlayViewer(
-    	url=ustate["url"],
-    	rectangles_csv="\n".join(currentrois)
-    )
-    overlayviewer
+@app.cell
+def _(gallery):
+    gallery
     return
 
 
-@app.cell(hide_code=True)
-def _(galleryblock):
-    galleryblock
+@app.cell
+def _(gallery, mo, viewer):
+    mo.hstack([
+        viewer,mo.md(""), gallery],
+               widths=[75, 5, 20]
+             )
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.Html("<br/><br/><br/><br/><br/><br/><br/><br/>")
+    mo.Html("<hr/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>")
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    ## Computation
+    # Computation
     """)
     return
 
@@ -114,217 +99,161 @@ def _(mo):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    ### Mocks for image package
+    ## Load text corpus
     """)
-    return
-
-
-@app.cell
-def _(dataclass):
-    @dataclass
-    class CitableIIIFService:
-        urlbase: str
-        extension: str
-
-    return (CitableIIIFService,)
-
-
-@app.cell
-def _(CitableIIIFService):
-    service = CitableIIIFService(urlbase = "https://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/", extension = "tif")
-    return (service,)
-
-
-@app.cell
-def _(CitableIIIFService, u):
-    def urn2info_url(urn: str,srvc: CitableIIIFService):
-        speclabel,spectype,ns,collection,objectcomponent =  u.split(":")
-        collectionid, collectionversion = collection.split(".")
-        return srvc.urlbase + ns + "/" + collectionid + "/" + collectionversion + "/" + objectcomponent + "." + srvc.extension + "/info.json"
-
-    return (urn2info_url,)
-
-
-@app.cell
-def _(CitableIIIFService):
-    def info_url2urn1(url: str, srvc: CitableIIIFService):
-        strip1 = url.replace(srvc.urlbase,"")
-        ns,coll,vers,imgid,junk = strip1.split("/")
-        suffix = f".{srvc.extension}"
-        objid = imgid.removesuffix(suffix)
-        return  "urn:cite2:" + ns + ":" + coll + "." + vers  + ":" + objid
-
-    def info_url2urn(url: str, srvc: CitableIIIFService):
-        return info_url2urn1(url, srvc)
-
-    return (info_url2urn,)
-
-
-@app.cell
-def _(info_url2urn, infourl_eg, service):
-    info_url2urn(infourl_eg,service)
     return
 
 
 @app.cell
 def _():
-    u = "urn:cite2:citebne:complutensian.v1:v1p19"
-    return (u,)
-
-
-@app.cell
-def _(service, u, urn2info_url):
-    infourl_eg = urn2info_url(u, service)
-    return (infourl_eg,)
-
-
-@app.cell
-def _(infourl_eg, mo):
-    mo.md(f"""
-    {infourl_eg}
-    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    ### Load DSE
+    ## Load DSE
     """)
     return
 
 
 @app.cell
+def _(DSE, alldse):
+    dse = DSE(alldse)
+    return (dse,)
+
+
+@app.cell
+def _(loadsource, lxxsrc, pl, targumsrc):
+    lxxdf = loadsource(lxxsrc)
+    targumdf = loadsource(targumsrc)
+    alldse = pl.concat([lxxdf, targumdf]).unique()
+    return (alldse,)
+
+
+@app.cell
 def _(mo):
-    datadir = mo.notebook_location() / "public"
-    return (datadir,)
+    lxxsrc = str(mo.notebook_dir() / "public" / "septuagint_latin_genesis_dse.cex")
+    targumsrc = str(mo.notebook_dir() / "public" / "targum_latin_genesis_dse.cex")
+    return lxxsrc, targumsrc
 
 
 @app.cell
-def _(datadir):
-    lxxfile = str(datadir / "septuagint_latin_genesis_dse.cex")
-    return (lxxfile,)
+def _(StringIO, lxxsrc, pl, urlopen):
+    def loadsource(src):
+        if src.startswith(("http://", "https://")):
+            with urlopen(src) as response:
+                csv_text = response.read().decode("utf-8")
+                return pl.read_csv(StringIO(csv_text), separator="|", quote_char=None)
+        else:
+            return pl.read_csv(lxxsrc,separator="|",quote_char=None)
 
-
-@app.cell
-def _(lxxfile):
-    lxxfile
-    return
-
-
-@app.cell
-def _(lxxfile, pl):
-    lxxdf = pl.read_csv( lxxfile, separator="|")
-    return (lxxdf,)
-
-
-@app.cell
-def _(DSE, lxxdf):
-    lxx = DSE(lxxdf)
-    return (lxx,)
+    return (loadsource,)
 
 
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    ### Clean these up
+    ## Wire coordinate tracking
     """)
     return
 
 
 @app.cell
-def _(pl):
-    def roi():
-        pl.col("image").str.extract(r"@((?:\d+,){3}\d+)$", 1)
-
-    return
-
-
-@app.cell
-def _(pl):
-    def getroi():
-        "Extract RoIs from image references"
-        pl.col("image").cast(pl.Utf8).str.replace(r"^.*@", "").alias("roi")
-
-
-    return (getroi,)
+def _(mo):
+    x_coord_state, set_x_coord_state = mo.state(0.0)
+    y_coord_state, set_y_coord_state = mo.state(0.0)
+    return set_x_coord_state, set_y_coord_state, x_coord_state, y_coord_state
 
 
 @app.cell
-def _(lxx, pl):
-    lxx.df = lxx.df.with_columns(
-        pl.col("image")
-          .cast(pl.Utf8)
-          .str.replace(r"@.*$", "")
-          .alias("alt")
-    )
-    return
+def _(x_coord_state, y_coord_state):
+    x_state = x_coord_state()
+    y_state = y_coord_state()
+    return x_state, y_state
 
 
 @app.cell
-def _(getroi, lxx):
-    roidf2 = lxx.df.with_columns(getroi())
-    return
+def _(set_x_coord_state, set_y_coord_state, viewer):
+    def push_coordinate_state(_change=None):
+        "Update normalized x/y values from gallery object."
+        next_x = getattr(viewer, "normalized_x", 0.0)
+        next_y = getattr(viewer, "normalized_y", 0.0)
+        if isinstance(_change, dict):
+            changed_name = _change.get("name")
+            if changed_name == "normalized_x":
+                next_x = _change.get("new")
+            elif changed_name == "normalized_y":
+                next_y = _change.get("new")
+
+        set_x_coord_state(next_x)
+        set_y_coord_state(next_y)
+
+    return (push_coordinate_state,)
 
 
 @app.cell
-def _(getroi, lxx):
-    roidf = lxx.df = lxx.df.with_columns(getroi())
-    #pl.col("image").cast(pl.Utf8).str.replace(r"^.*@", "").alias("roi")
-    #)
+def _(push_coordinate_state, viewer):
+    if viewer is not None:
+        previous_image_observer = getattr(viewer, "_marimo_coord_observer", None)
+        if previous_image_observer:
+            viewer.unobserve(previous_image_observer, names=["normalized_x", "normalized_y"])
+        viewer.observe(push_coordinate_state, names=["normalized_x", "normalized_y"])
+        viewer._marimo_coord_observer = push_coordinate_state
+        push_coordinate_state()
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    ### Set up variable observer
+    ## Wire tracking image selection
     """)
     return
 
 
 @app.cell
-def _(url_state):
-    ustate = url_state()
-    return (ustate,)
+def _(image_state):
+    imgstate = image_state()
+    return (imgstate,)
 
 
 @app.cell
 def _(mo):
-    url_state, set_url_state = mo.state(
-        {
-            "url": ""
-        }
-    )
-    return set_url_state, url_state
+    image_state, set_image_state = mo.state("")
+    return image_state, set_image_state
 
 
 @app.cell
-def _(push_state, viewer):
-    if viewer:
-        viewer.observe(push_state, names=["url"])
-        viewer._marimo_observer = push_state
-        push_state()
-    return
-
-
-@app.cell
-def _(set_url_state, viewer):
-    def push_state(_change=None):
+def _(gallery, set_image_state):
+    def push_image_state(_change=None):
         "Update value of URL from viewer object."
-        set_url_state(
-            {
-                "url": viewer.url
-            }
-        )
+        selected_info_url = ""
+        if isinstance(_change, dict):
+            selected_info_url = (_change.get("new") or "").strip()
+        if not selected_info_url:
+            selected_info_url = (gallery.selected_info_url or "").strip()
+        set_image_state(selected_info_url)
 
-    return (push_state,)
+    return (push_image_state,)
+
+
+@app.cell
+def _(gallery, push_image_state):
+    if gallery is not None:
+        previous_observer = getattr(gallery, "_marimo_observer", None)
+        if previous_observer:
+            gallery.unobserve(previous_observer, names=["selected_info_url"])
+        gallery.observe(push_image_state, names=["selected_info_url"])
+        gallery._marimo_observer = push_image_state
+        push_image_state()
+    return
 
 
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    ### Layout
+    ## Image displays
     """)
     return
 
@@ -338,45 +267,141 @@ def _(IIIFThumbnailGallery, manifest):
 
 
 @app.cell
-def _(gallery):
-    image_choice = None
-    if gallery:
-        image_choice = gallery.selected_info_url.strip()
-    return (image_choice,)
+def _(currentrects):
+    currentrects
+    return
 
 
 @app.cell
-def _(IIIFViewer, image_choice, imageheight):
+def _(IIIFImageOverlayViewer, currentrects, imgstate):
     viewer = None
-    if image_choice:
-        viewer = IIIFViewer(url = image_choice, height=imageheight.value)
+    if imgstate:
+        viewer = IIIFImageOverlayViewer(
+            url = imgstate,rectangles_csv=currentrects)
     return (viewer,)
 
 
 @app.cell
-def _(gallery, mo, viewer):
-    galleryblock = None
-    if viewer:    
-        galleryblock = mo.hstack([
-            viewer,
-            mo.md(""),
-            gallery
-        ], widths=[75, 5, 20])
-    return (galleryblock,)
+def _(CitableIIIFService):
+    service = CitableIIIFService(urlbase = "https://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/", extension = "tif")
+    return (service,)
 
 
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    ### User selection
+    ## Current DSE selections
     """)
     return
 
 
 @app.cell
+def _(imgstate, info_url2urn, service):
+    currentimg = info_url2urn(imgstate,service)
+    return (currentimg,)
+
+
+@app.cell
+def _(currentimg):
+    currentimg
+    return
+
+
+@app.cell
+def _(currentimg, dse):
+    pgs = dse.surfacesforimage(currentimg)
+    pgs
+    return (pgs,)
+
+
+@app.cell
+def _(pgs):
+    pgs.is_empty()
+    return
+
+
+@app.cell
+def _(pgs):
+    currentdsepage = pgs[0,0]
+    return (currentdsepage,)
+
+
+@app.cell
+def _(currentdsepage):
+    currentdsepage
+    return
+
+
+@app.cell
+def _(currentdsepage, dse, pgs, pl):
+    if pgs.is_empty():
+        pagedf = pgs
+    else:
+        pagedf = dse.df.filter(pl.col("surface") == currentdsepage)
+    return (pagedf,)
+
+
+@app.cell
+def _():
+    #pagedf
+    return
+
+
+@app.cell
+def _(pagedf, rois):
+    if pagedf.is_empty():
+        roilist = []
+    else:
+        roilist = rois(pagedf)
+    return (roilist,)
+
+
+@app.cell
+def _(roilist):
+    currentrects =  "\n".join(roilist)
+    return (currentrects,)
+
+
+@app.cell
+def _(currentdsepage, mo):
+    mo.md(f"Current page: `{currentdsepage}`")
+    return
+
+
+@app.cell
+def _(currentrects, mo):
+    mo.md(f"Current rects: {currentrects}")
+    return
+
+
+@app.cell
+def _(pagedf, ptinrect, x_state, y_state):
+    if pagedf.is_empty():
+        clickedon = pagedf
+    else:
+        clickedon = pagedf.filter(ptinrect(x_state, y_state))
+    
+    return (clickedon,)
+
+
+@app.cell
+def _(clickedon):
+    clickedon
+    return
+
+
+@app.cell
+def _(clickedon, pl):
+    currentpsgreff = clickedon.select("passage").filter(pl.col("passage").is_not_null()).to_series().to_list()
+    return (currentpsgreff,)
+
+
+@app.cell(hide_code=True)
 def _(mo):
-    imageheight = mo.ui.slider(start=100,stop=1500,value=600,show_value=True,label="*Height of image (pixels)*:")
-    return (imageheight,)
+    mo.md("""
+    ## User selections
+    """)
+    return
 
 
 @app.cell
@@ -400,7 +425,7 @@ def _():
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    ### Imports
+    ## Imports
     """)
     return
 
@@ -409,7 +434,7 @@ def _(mo):
 def _():
     from iiif_anywidget import IIIFViewer, IIIFThumbnailGallery, IIIFImageOverlayViewer
 
-    return IIIFImageOverlayViewer, IIIFThumbnailGallery, IIIFViewer
+    return IIIFImageOverlayViewer, IIIFThumbnailGallery
 
 
 @app.cell
@@ -421,16 +446,25 @@ def _():
 
 @app.cell
 def _():
-    from dse_polars import DSE
+    from dse_polars import DSE, CitableIIIFService, info_url2urn, urn2image_url, rois, ptinrect
+    from dse_polars.texts import textcontents
 
-    return (DSE,)
+    return CitableIIIFService, DSE, info_url2urn, ptinrect, rois
 
 
 @app.cell
 def _():
     from dataclasses import dataclass
 
-    return (dataclass,)
+    return
+
+
+@app.cell
+def _():
+    from io import StringIO
+    from urllib.request import urlopen
+
+    return StringIO, urlopen
 
 
 if __name__ == "__main__":
